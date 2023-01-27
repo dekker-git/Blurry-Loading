30dd59ecc39e2e91cb4c6fbfa53d64619b1e9c4a
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-27 10:48:34 -0700

    created function to dynamically change loading% and blur%

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 
 let load = 0;
-let int = setInterval(blurring, 30);
+let int = setInterval(blurring, 50);
 function blurring() {
-    if (load < 100) load++;
-     else return
+    if (load < 100) {
+        load++;
+        loadText.innerHTML = load + '%';
+    } else clearInterval(int);
+     loadText.style.opacity = 1 - load/100;
+     bg.style.filter = `blur(${90-load}px)`;
     console.log(load);
-}
+    console.log('done');
+};
+
+(function some () {
+console.log('hello');
+}());
