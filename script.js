6524453bb17717ddeed3c3c6fe12bbda434c2386
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-27 10:30:55 -0700

    load function started in js, need to manipulate DOM with it

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+let int = setInterval(blurring, 30);
+function blurring() {
+    if (load < 100) load++;
+     else return
+    console.log(load);
+}
